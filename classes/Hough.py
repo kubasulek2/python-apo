440d4f9264d162b5c7dd9a13fab4452dbd07196d
Kuba Sułkowski <jakub.sulkowski@pragmaticcoders.com>
2024-02-11 14:20:35 +0100

    Add documentation

--- classes/Hough.py
+++ classes/Hough.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 
 class Hough(ImageOperation):
-    def transform(self, threshold=200):
+    def transform(self, threshold=100):
         img, edges = self.prepare_image()
         lines = cv2.HoughLines(edges, 1, np.pi / 180, threshold)
         if lines is None:
@@ -21,7 +21,7 @@ class Hough(ImageOperation):
                 y1 = int(y0 + 1000 * a)
                 x2 = int(x0 - 1000 * -b)
                 y2 = int(y0 - 1000 * a)
-                cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                cv2.line(img, (x1, y1), (x2, y2), (0, 0, 0), 2)
         return Image.fromarray(img)
 
     def transform_p(self, threshold=100, min_line_length=100, max_line_gap=10):
@@ -32,13 +32,14 @@ class Hough(ImageOperation):
             return Image.fromarray(img)
         for line in lines:
             for x1, y1, x2, y2 in line:
-                cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.line(img, (x1, y1), (x2, y2), (0, 0, 0), 2)
 
         return Image.fromarray(img)
 
     def prepare_image(self):
         img = np.array(self.image)
         if len(img.shape) == 3:
+            print("Converting to gray")
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         else:
             gray = img
